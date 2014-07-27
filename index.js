0376c81ea4fe99efc384189ec19edc73e13909a1
Anton Wilhelm <timaschew@users.noreply.github.com>
2014-07-27 22:39:05 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+var jade = require('jade');
+
+var extend = function(target, source) {
+    var prop, val;
+    if (!(target instanceof Object)) {
+        throw new Error("" + target + " is not a object");
+    }
+    for (prop in source) {
+        val = source[prop];
+        target[prop] = val;
+    }
+    return target;
+};
+
+module.exports = function(compiledJade, globals) {
+    var template;
+    template = compiledJade(jade);
+    if (globals != null) {
+        return function(locals) {
+            extend(globals, locals);
+            return template(globals);
+        };
+    } else {
+        return function(locals) {
+            return template(locals || {});
+        };
+    }
+};
